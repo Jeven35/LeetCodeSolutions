48d8646b64912ea972096b08e3e3c65a030600b9
jeven <161250109@smail.nju.edu.cn>
2019-05-22 19:33:50 +0800

    添加测试

---
+++ src/java/Solution.java
@@ -0,0 +1,15 @@
+package java;
+
+/**
+ * Created by jeven on 2019/5/22.
+ */
+public class Solution {
+
+
+    /**
+     * 3.无重复字符的最长子串
+     */
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,34 @@
+package test.java;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * Solution Tester.
+ *
+ * @author <Authors name>
+ * @since <pre>���� 22, 2019</pre>
+ * @version 1.0
+ */
+public class SolutionTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     *
+     * Method: lengthOfLongestSubstring(String s)
+     *
+     */
+    @Test
+    public void testLengthOfLongestSubstring() throws Exception {
+    }
+
+
+} 
