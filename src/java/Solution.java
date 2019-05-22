fd73f0e9b9f319fd14e028b80d55fc62d2550ece
jeven <161250109@smail.nju.edu.cn>
2019-05-22 20:11:07 +0800

    完成第三题（滑动窗口）

--- src/java/Solution.java
+++ src/java/Solution.java
@@ -1,5 +1,8 @@
 package java;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jeven on 2019/5/22.
  */
@@ -8,8 +11,20 @@ public class Solution {
 
     /**
      * 3.无重复字符的最长子串
+     * 思路：滑动窗口
      */
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        Map<Character,Integer> map = new HashMap();
+        int left = 0;
+        int max = 0;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+                left = Math.max(left,map.get(c)+1);
+            }
+            map.put(c,i);
+            max = Math.max(max,i+1-left);
+        }
+        return max;
     }
 }
